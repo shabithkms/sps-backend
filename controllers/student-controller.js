ec12cda0282140c6ab716eb75205dd302bb7162d
shabithkms <shabithkms2035@gmail.com>
2022-02-14 16:42:40 +0530

    edit student data completed

--- controllers/student-controller.js
+++ controllers/student-controller.js
@@ -1,7 +1,9 @@
-const collection = require("../config/collection");
-const db = require("../config/connection");
-const bcrypt = require("bcryptjs");
-const { response } = require("express");
+const collection = require('../config/collection');
+const db = require('../config/connection');
+const bcrypt = require('bcryptjs');
+const { ObjectId } = require('mongodb');
+const { uploadFile } = require('../utils/s3');
+const fs = require('fs');
 
 module.exports = {
   doSignup: (req, res) => {
@@ -33,16 +35,16 @@ module.exports = {
             .then((response) => {
               return res
                 .status(200)
-                .json({ message: "Registered successfully" });
+                .json({ message: 'Registered successfully' });
             });
         } else {
           return res
             .status(401)
-            .json({ errors: "You are not selected for SPS" });
+            .json({ errors: 'You are not selected for SPS' });
         }
       } catch (error) {
         console.log(error);
-        res.status(500).json({ errors: "Something error" });
+        res.status(500).json({ errors: 'Something error' });
       }
     });
   },
@@ -62,28 +64,118 @@ module.exports = {
             delete student.hashedPassword;
             return res
               .status(200)
-              .json({ message: "Signed in successfully", student });
+              .json({ message: 'Signed in successfully', student });
           } else {
-            return res.status(401).json({ errors: "Incorrect password" });
+            return res.status(401).json({ errors: 'Incorrect password' });
           }
         } else {
-          return res.status(404).json({ errors: "Student doesnot exist" });
+          return res.status(404).json({ errors: 'Student doesnot exist' });
         }
       } catch (error) {
         console.log(error);
-        res.status(500).json({ errors: "Something error" });
+        res.status(500).json({ errors: 'Something error' });
       }
     });
   },
   editProfile: (req, res) => {
-    return new Promise((resolve, reject) => {
-      console.log(req.body);
+    return new Promise(() => {
+      const file = req.file;
       try {
-        db.get().collection(collection.STUDENT_COLLECTION).updateOne({});
+        const {
+          Name,
+          Domain,
+          DOB,
+          Age,
+          Gender,
+          Email,
+          Mobile,
+          FatherName,
+          FatherNo,
+          MotherName,
+          MotherNo,
+          Guardian,
+          Relationship,
+          Address,
+          Village,
+          Taluk,
+          Qualification,
+          School,
+          Experience,
+          PaymentMethod,
+        } = req.body;
+        db.get()
+          .collection(collection.STUDENT_COLLECTION)
+          .updateOne(
+            { Email },
+            {
+              $set: {
+                Name,
+                Domain,
+                DOB,
+                Age,
+                Gender,
+                Mobile,
+                FatherName,
+                FatherNo,
+                MotherName,
+                MotherNo,
+                Guardian,
+                Relationship,
+                Address,
+                Village,
+                Taluk,
+                Qualification,
+                School,
+                Experience,
+                PaymentMethod,
+              },
+            }
+          )
+          .then(async (response) => {
+            console.log(response);
+            if (file) {
+              const fileName = file.filename;
+              const result = await uploadFile(file);
+              fs.unlinkSync(`uploads/${fileName}`);
+              console.log('file deleted');
+
+              db.get()
+                .collection(collection.STUDENT_COLLECTION)
+                .updateOne(
+                  { Email },
+                  {
+                    $set: {
+                      ID_Proof: result.Location,
+                    },
+                  }
+                );
+            }
+
+            let student = await db
+              .get()
+              .collection(collection.STUDENT_COLLECTION)
+              .findOne({ Email });
+            res.status(200).json({ message: 'updated successfully', student });
+          });
       } catch (error) {
         console.log(error);
-        return res.status(500).json({ errors: "Sometthing error" });
+        return res.status(500).json({ errors: error.message });
       }
     });
+  },
+  getStudentData: async (req, res) => {
+    try {
+      let { id } = req.params;
+      console.log('id', id);
+      const student = await db
+        .get()
+        .collection(collection.STUDENT_COLLECTION)
+        .findOne({ _id: ObjectId(id) });
+      console.log(student);
+      return res.status(200).json({ student });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ errors: error.message });
+    }
   },
 };
--- routes/student.js
+++ routes/student.js
@@ -1,12 +1,19 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const studentController = require("../controllers/student-controller");
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const studentController = require('../controllers/student-controller');
 
 // Authentication
-router.post("/signup", studentController.doSignup);
-router.post("/login", studentController.doLogin);
+router.post('/signup', studentController.doSignup);
+router.post('/login', studentController.doLogin);
 
 // Profile Management
-router.post("/editProfile", studentController.editProfile);
+router.post(
+  '/editProfile',
+  upload.single('ID_Proof'),
+  studentController.editProfile
+);
+router.get('/getStudentData/:id', studentController.getStudentData);
 
 module.exports = router;
