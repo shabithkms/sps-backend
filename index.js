3a24a565d8fdcd7d75cf1483fac9703cf3f8865c
shabithkms <shabithkms2035@gmail.com>
2022-02-19 11:58:50 +0530

    code reformatting eith eslint

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const express = require("express");
-require("dotenv").config();
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const express = require('express');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const app = express();
+const db = require('./config/connection');
 
-var adminRouter = require("./routes/admin");
-var studentRouter = require("./routes/student");
-var teacherRouter = require("./routes/teacher");
-var reviewerRouter = require("./routes/reviewer");
+var adminRouter = require('./routes/admin');
+var studentRouter = require('./routes/student');
+var teacherRouter = require('./routes/teacher');
+var reviewerRouter = require('./routes/reviewer');
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
-});
+}); 
 
 //Middlewaress
-app.use(express.json({ limit: "50mb", extended: true }));
-app.use(express.urlencoded({ limit: "50mb", extended: false }));
+app.use(express.json({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
     credentials: true,
   })
 );
 
-app.get('/api',(req,res)=>{
-  res.send("Welcome to SPS Api service")
-})
+app.get('/api', (req, res) => {
+  res.send('Welcome to SPS Api service');
+});
 
 //Database connection
-var db = require("./config/connection");
 db.connect((err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("Database connected");
+    console.log('Database connected');
   }
 });
 
 //Routes
-app.use("/", studentRouter);
-app.use("/admin", adminRouter);
-app.use("/teacher", teacherRouter);
-app.use("/reviewer", reviewerRouter);
+app.use('/', studentRouter);
+app.use('/admin', adminRouter);
+app.use('/teacher', teacherRouter);
+app.use('/reviewer', reviewerRouter);
 
 module.exports = app;
